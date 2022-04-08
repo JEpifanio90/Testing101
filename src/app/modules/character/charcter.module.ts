911c16a60a5114dfc8d5a39704248e3af31c559f
Jose Epifanio <jose.epifanio90@gmail.com>
2022-04-08 00:03:07 -0400

    Fix linting

--- src/app/modules/character/charcter.module.ts
+++ src/app/modules/character/charcter.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { CharcterRoutingModule } from './charcter-routing.module';
+import { CharcterRoutingModule } from "./charcter-routing.module";
 
 
 @NgModule({
@@ -11,4 +11,5 @@ import { CharcterRoutingModule } from './charcter-routing.module';
     CharcterRoutingModule
   ]
 })
-export class CharcterModule { }
+export class CharcterModule {
+}
