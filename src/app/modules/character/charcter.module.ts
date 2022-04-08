faaafedf5af024f0969d77f48e94da2dc9e26097
Jose Epifanio <jose.epifanio90@gmail.com>
2022-04-08 00:02:26 -0400

    Add dashboard and character module

---
+++ src/app/modules/character/charcter.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CharcterRoutingModule } from './charcter-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CharcterRoutingModule
+  ]
+})
+export class CharcterModule { }
