e94a751b21768c669f71aa260e046495c7287c10
Jose Epifanio <jose.epifanio90@gmail.com>
2022-04-08 00:54:46 -0400

    Fix component spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,16 @@
-import { TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
+import { MockInstance, ngMocks } from "ng-mocks";
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from "@project/app.component";
 
-describe("AppComponent", () => {
+describe("App Component", () => {
+  ngMocks.faster();
+  MockInstance.scope();
+
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -13,24 +20,12 @@ describe("AppComponent", () => {
         AppComponent
       ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   it("should create the app", () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it("should have as title 'Testing101'", () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual("Testing101");
-  });
-
-  it("should render title", () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector(".content span")?.textContent).toContain("Testing101 app is running!");
+    expect(component).toBeTruthy();
   });
 });
